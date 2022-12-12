0c05800e4b6ebb8bafe0f786ced6b61f763a4e73
isukakori <1169153441@qq.com>
2022-12-12 17:37:01 +0800

    test

---
+++ src/main/java/com/xxxx/base/utils/AssertUtil.java
@@ -0,0 +1,14 @@
+package com.xxxx.base.utils;
+
+import com.xxxx.crm.exceptions.ParamsException;
+
+public class AssertUtil {
+
+
+    public  static void isTrue(Boolean flag,String msg){
+        if(flag){
+            throw  new ParamsException(msg);
+        }
+    }
+
+}
